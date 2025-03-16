ef454c32b5182c2f446bc5a5f8e98858ef1973ea
Colin Payne-Rogers <cpaynerogers@magnite.com>
2025-03-16 15:07:29 -0400

    Initial plotting module

---
+++ Code/src/plotting/colors.py
@@ -0,0 +1,18 @@
+TD_GREEN = "#008a00"
+TD_GREEN_TETRAD = {
+    "green": TD_GREEN,
+    "blue": "#00458a",
+    "purple": "#8a008a",
+    "brown": "#8a4500",
+}
+COLORS = TD_GREEN_TETRAD
+
+# The TD colors tend to be sharp, so there is also a more luminant and
+# less saturated (from closer to the center of the color wheel) set for
+# use here:
+SOFT_COLORS = {
+    "green": "#9AC59A",
+    "blue": "#96ADC3",
+    "purple": "#C79FC7",
+    "brown": "#CFBDAB",
+}
