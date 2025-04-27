7b0b922ad50fb50f52ef0e298f3d816e640a93d9
sophiewagner7 <sophie.wagn@gmail.com>
2025-04-27 10:01:29 -0400

    move trainer to model/init

--- Code/src/model/__init__.py
+++ Code/src/model/__init__.py
@@ -1,3 +1,4 @@
+from types import SimpleNamespace
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -88,11 +89,9 @@ class GNNTrainer:
     """Trainer class for GINe-based Graph Neural Network using PyTorch Geometric and torchmetrics.
     Handles training, evaluation, early stopping, and metric logging.
     """
-    # TODO: make this easier so we can just pass in pl object, and it uses these things from pl? 
-    def __init__(self, model, train_loader, val_loader, test_loader,
-                 train_indices, val_indices, test_indices, df,
+    def __init__(self, model, pl, threshold: float=0.5,
                  device ="cuda" if torch.cuda.is_available() else "cpu", 
-                 pos_weight_val=6.0, lr=0.005):
+                 pos_weight_val:float=6.0, lr:float=0.005):
         """Initializes the trainer with model, data loaders, and training parameters.
 
         Args:
@@ -104,18 +103,18 @@ class GNNTrainer:
             pos_weight_val (float, optional): Positive class weight for BCEWithLogitsLoss. Defaults to 6.0 (what multi-gnn repo used).
             lr (float, optional): Learning rate for Adam optimizer. Defaults to 0.005.
         """
-        
-        self.device = device
+        self.device = pl.device
         self.model = model.to(self.device)
-        self.df = df
+        self.threshold = pl.threshold
+        self.epochs = pl.epochs
         
         # Data
-        self.train_loader = train_loader
-        self.val_loader = val_loader
-        self.test_loader = test_loader
-        self.train_indices = train_indices
-        self.val_indices = val_indices
-        self.test_indices = test_indices
+        self.train_loader = pl.train_loader
+        self.val_loader = pl.val_loader
+        self.test_loader = pl.test_loader
+        self.train_indices = pl.train_indices
+        self.val_indices = pl.val_indices
+        self.test_indices = pl.test_indices
 
         # Loss and optimizer
         self.criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([pos_weight_val], device=self.device))
@@ -128,8 +127,17 @@ class GNNTrainer:
             verbose=True
         )
         
+        # Metrics used during training and evaluation
+        self.metrics = SimpleNamespace(
+            accuracy=BinaryAccuracy(threshold=threshold).to(self.device),
+            precision=BinaryPrecision(threshold=threshold).to(self.device),
+            recall=BinaryRecall(threshold=threshold).to(self.device),
+            f1_score=BinaryF1Score(threshold=threshold).to(self.device),
+            pr_auc=BinaryAveragePrecision().to(self.device),
+        )
+        
     @torch.no_grad()
-    def evaluate(self, loader, inds, threshold):
+    def evaluate(self, loader, inds):
         """Evaluate model performance on a given data loader and indices.
 
         Args:
@@ -143,11 +151,11 @@ class GNNTrainer:
         self.model.eval() # evaluation mode
         
         # Initialize metrics
-        acc_fn = BinaryAccuracy(threshold=threshold).to(self.device)
-        prec_fn = BinaryPrecision(threshold=threshold).to(self.device)
-        rec_fn = BinaryRecall(threshold=threshold).to(self.device)
-        f1_fn = BinaryF1Score(threshold=threshold).to(self.device)
-        pr_auc_fn = BinaryAveragePrecision().to(self.device)
+        self.metrics.accuracy.reset()
+        self.metrics.precision.reset()
+        self.metrics.recall.reset()
+        self.metrics.f1_score.reset()
+        self.metrics.pr_auc.reset()
 
         loss_fn = nn.BCEWithLogitsLoss()
         preds, targets, probs = [], [], []
@@ -163,14 +171,14 @@ class GNNTrainer:
             mask = torch.isin(torch.tensor(edge_ids_in_batch), torch.tensor(seed_edge_ids)).to(self.device)
 
             # Remove edge_id from attributes (now that we have identified seed ids)
-            batch_edge_attr = batch.edge_attr[:, 1:].clone()
             batch = batch.to(self.device)
+            batch_edge_attr = batch.edge_attr[:, 1:].clone()
             
             # Forward pass
             logits = self.model(batch.x, batch.edge_index, batch_edge_attr).view(-1)[mask]
             target = batch.y[mask]
             prob = torch.sigmoid(logits)
-            pred = (prob > threshold).long()
+            pred = (prob > self.threshold).long()
 
             total_loss += loss_fn(logits, target.float()).item() * logits.size(0)
             
@@ -187,14 +195,14 @@ class GNNTrainer:
 
         return (
             total_loss,
-            acc_fn(preds, targets),
-            prec_fn(preds, targets),
-            rec_fn(preds, targets),
-            f1_fn(preds, targets),
-            pr_auc_fn(probs, targets)
+            self.metrics.accuracy(preds, targets),
+            self.metrics.precision(preds, targets),
+            self.metrics.recall(preds, targets),
+            self.metrics.f1_score(preds, targets),
+            self.metrics.pr_auc(probs, targets)
         )
 
-    def train(self, threshold=0.5, epochs=20, patience=10):
+    def train(self):
         """Main training loop for the model.
 
         Args:
@@ -202,30 +210,23 @@ class GNNTrainer:
             epochs (int): Number of max training epochs.
             patience (int): Early stopping patience after min_epochs is reached.
         """
-        
-        # Initialize metrics
-        acc_fn = BinaryAccuracy(threshold=threshold).to(self.device)
-        prec_fn = BinaryPrecision(threshold=threshold).to(self.device)
-        rec_fn = BinaryRecall(threshold=threshold).to(self.device)
-        f1_fn = BinaryF1Score(threshold=threshold).to(self.device)
-        pr_auc_fn = BinaryAveragePrecision().to(self.device)
 
         best_val_f1 = 0
         best_pr_auc = 0
         patience_counter = 0  # for early stopping
-        min_epochs = 10       # Force model to train at least this long
+        min_epochs = 10       # don't allow early model saving
 
-        for epoch in range(epochs):
+        for epoch in range(self.epochs):
             self.model.train()
             train_loss = 0
             train_preds, train_targets, train_probs = [], [], []
             
             # Reset metrics
-            acc_fn.reset()
-            prec_fn.reset()
-            rec_fn.reset()
-            f1_fn.reset()
-            pr_auc_fn.reset()
+            self.metrics.accuracy.reset()
+            self.metrics.precision.reset()
+            self.metrics.recall.reset()
+            self.metrics.f1_score.reset()
+            self.metrics.pr_auc.reset()
 
             for batch in tqdm(self.train_loader, desc=f"Epoch {epoch+1} Training"):
                 self.optimizer.zero_grad()
@@ -238,14 +239,14 @@ class GNNTrainer:
                 mask = torch.isin(torch.tensor(edge_ids_in_batch), torch.tensor(seed_edge_ids)).to(self.device)
 
                 # Remove edge_id as attribute before running model
-                batch_edge_attr = batch.edge_attr[:, 1:].clone()
                 batch = batch.to(self.device)
+                batch_edge_attr = batch.edge_attr[:, 1:].clone()
                 
                 # Forward pass
                 logits = self.model(batch.x, batch.edge_index, batch_edge_attr).view(-1)[mask]
                 target = batch.y[mask]
                 probs = torch.sigmoid(logits)
-                preds = (probs > threshold).long()
+                preds = (probs > self.threshold).long()
                 
                 # Loss and backpropagation
                 loss = self.criterion(logits, target.float())
@@ -265,28 +266,26 @@ class GNNTrainer:
             train_probs = torch.cat(train_probs)
             train_loss /= len(train_targets)
 
-            train_acc = acc_fn(train_preds, train_targets)
-            train_prec = prec_fn(train_preds, train_targets)
-            train_rec = rec_fn(train_preds, train_targets)
-            train_f1 = f1_fn(train_preds, train_targets)
-            train_pr_auc = pr_auc_fn(train_probs, train_targets)
+            train_acc = self.metrics.accuracy(train_preds, train_targets)
+            train_prec = self.metrics.precision(train_preds, train_targets)
+            train_rec = self.metrics.recall(train_preds, train_targets)
+            train_f1 = self.metrics.f1_score(train_preds, train_targets)
+            train_pr_auc = self.metrics.pr_auc(train_probs, train_targets)
 
             # Validation
             val_loss, val_acc, val_prec, val_rec, val_f1, val_pr_auc = self.evaluate(
                 self.val_loader,
-                self.val_indices,
-                threshold,
+                self.val_indices
             )
 
             # Test
             test_loss, test_acc, test_prec, test_rec, test_f1, test_pr_auc = self.evaluate(
                 self.test_loader,
                 self.test_indices,
-                threshold,
             )
 
             # Logging
-            logging.info(f"Epoch {epoch+1}/{epochs}")
+            logging.info(f"Epoch {epoch+1}/{self.epochs}")
             logging.info(f"Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | Test Loss: {test_loss:.4f}")
             logging.info(f"Train Acc: {train_acc:.4f} | Val Acc: {val_acc:.4f} | Test Acc: {test_acc:.4f}")
             logging.info(f"Train F1: {train_f1:.4f} | Val F1: {val_f1:.4f} | Test F1: {test_f1:.4f}")
@@ -307,8 +306,8 @@ class GNNTrainer:
                 print("✅ New best model saved.")
             elif epoch >= min_epochs:
                 patience_counter += 1
-                print(f"⚠️ No improvement. Patience: {patience_counter}/{patience}")
-                if patience_counter >= patience:
+                print(f"⚠️ No improvement. Patience: {patience_counter}/{self.patience}")
+                if patience_counter >= self.patience:
                     print("🛑 Early stopping triggered.")
                     break
 
